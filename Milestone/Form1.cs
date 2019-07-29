d4e923838b056cfbee3c3f9b82e91fc614b57025
Matthew Baker <MBaker66@my.gcu.edu>
2019-07-29 13:57:18 -0700

    Inventory manager class, load function that loads the items to an array of inventory

--- Milestone/Form1.cs
+++ Milestone/Form1.cs
@@ -44,36 +44,13 @@ namespace Milestone
         private void Form_products_Load(object sender, EventArgs e)
         {
             lb_products.Items.Add("Stock" + "\t" + "Model");
-
-            try
+            InventoryManager im = new InventoryManager();
+            Inventory[] inv = im.Load();
+            lb_products.Items.Add(inv[0].Price);
+            for (int x = 0; x < inv.Length; x++) 
             {
-                String[] input = File.ReadAllLines("inventory.txt");
-                int arrayLength = input.Length;
-                Inventory[] inv = new Inventory[arrayLength];
-
-                for (int x = 0; x < input.Length; x++)
-                {
-                    String[] splitInput = input[x].Split(',');
-                    int id = int.Parse(splitInput[0]);
-                    String name = splitInput[1];
-                    String model = splitInput[2];
-                    int stock = int.Parse(splitInput[3]);
-                    double price = double.Parse(splitInput[4]);
-                    double size = double.Parse(splitInput[5]);
-                    String rez = splitInput[6];
-
-                    inv[x] = new Inventory(id, name, model, stock, price, size, rez);
-
-                    lb_products.Items.Add(inv[x].Stock + "\t" + inv[x].Model);
-
-                    Console.WriteLine(inv[x].Price);
-                }
+                lb_products.Items.Add(inv[x].Stock + "\t" + inv[x].Model);
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            
         }
         public Inventory GetSelection
         {
--- Milestone/InventoryManager.cs
+++ Milestone/InventoryManager.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Milestone
 {
     class InventoryManager
     {
+        public Inventory[] Load()
+        {
+            try
+            {
+                String[] input = File.ReadAllLines("inventory.txt");
+                int arrayLength = input.Length;
+                Inventory[] inv = new Inventory[arrayLength];
 
+                for (int x = 0; x < input.Length; x++)
+                {
+                    String[] splitInput = input[x].Split(',');
+                    int id = int.Parse(splitInput[0]);
+                    String name = splitInput[1];
+                    String model = splitInput[2];
+                    int stock = int.Parse(splitInput[3]);
+                    double price = double.Parse(splitInput[4]);
+                    double size = double.Parse(splitInput[5]);
+                    String rez = splitInput[6];
+
+                    inv[x] = new Inventory(id, name, model, stock, price, size, rez);
+                }
+                return inv;
+            } catch (Exception) {
+                return null;
+            }
+        }
     }
 }
